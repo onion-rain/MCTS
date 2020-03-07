712eef7a4e45f73e8222173b9b3689b8cfc20063
onion-rain <1934455602@qq.com>
2020-03-07 12:21:43 +0800

    搞定收工

--- dump_model.py
+++ dump_model.py
@@ -1,26 +1,37 @@
 # 该文件通常用来加载.pth.tar文件以供debug
 
 import torch
+
+from utils import print_flops_params
+
 import warnings
 warnings.filterwarnings(action="ignore", category=UserWarning)
 
 import models
 
 def dump_model(model_name=None,
-                model_structure=None,
+                cfg=None,
                 checkpoint_path=None,
                 num_classes=10,
-                save_path=None):
+                save_path=None,
+                dataset='cifar10'):
     """
     从.pth.tar中取出state.dict并连同模型保存为.pth
     """
     checkpoint = torch.load(checkpoint_path)
-    if model_structure is None:
-        model_structure = checkpoint['structure']
-        print(model_structure)
-    model = models.__dict__[model_name](structure=model_structure, num_classes=num_classes)
+    if cfg is None:
+        if 'cfg' in checkpoint:
+            cfg = checkpoint['cfg']
+    model = models.__dict__[model_name](cfg=cfg, num_classes=num_classes)
     model.load_state_dict(checkpoint['model_state_dict'])
-    
+
+    for key in checkpoint:
+        if key != 'model_state_dict' and key != 'optimizer_state_dict':
+            print("{}: {}".format(key, checkpoint[key]))
+
+        
+    print_flops_params(model, dataset)
+
     if save_path is not None:
         torch.save(model, save_path)
 
@@ -28,9 +39,10 @@ def dump_model(model_name=None,
 
 if __name__ == "__main__":
     dump_model(
-        model_name='vgg19_bn_cifar', 
-        model_structure=None,
-        checkpoint_path='checkpoints/cifar10_vgg19_bn_cifar_sr_refine_best.pth.tar',
+        model_name='resnet110_cs', 
+        cfg=None,
+        checkpoint_path='checkpoints/no_sparsity/cifar10_resnet110_cs_best.pth.tar',
         # num_classes=10,
         # save_path='VGG19BN_slimmed0.7_10.0.pth'
+        dataset="cifar10"
     )
